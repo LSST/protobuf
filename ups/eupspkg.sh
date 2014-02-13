1ec0f6d55a78149f8ed51ac4e60258edc36959b7
Mario Juric <mjuric@lsst.org>
2014-02-13 13:15:51 -0600

    Transitioned eupspkg to eupspkg.sh

---
+++ ups/eupspkg.sh
@@ -0,0 +1,19 @@
+# EupsPkg config file. Sourced by 'eupspkg'
+
+build()
+{
+    default_build
+
+    cd python
+    python setup.py build
+}
+
+install()
+{
+    default_install
+
+    PYDEST="$PREFIX/lib/python"
+    mkdir -p "$PYDEST"
+    cd python
+    PYTHONPATH="$PYDEST:$PYTHONPATH" python setup.py install $PYSETUP_INSTALL_OPTIONS
+}
